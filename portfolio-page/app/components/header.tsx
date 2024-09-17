2545017ded04a949b2c80bd51e2d79c48c132354
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-09-17 11:28:05 +0000

    (feature/header):Complete

--- portfolio-page/app/components/header.tsx
+++ portfolio-page/app/components/header.tsx
@@ -5,10 +5,10 @@ export default function Header(){
         <header className='text-gray-700 border-b border-gray-200'>
             <div className='container flex flex-col mx-auto p-5 md:flex-row items-center'>
                 <a href='#' className='font-medium text-gray-900 mb-4 md:mb-0'>
-                    <span className='text-xl ml-3'>codsalmoon</span>
+                    <span className='text-xl ml-0 md:ml-3'>codsalmoon</span>
                 </a>
                 <nav className='md:ml-auto text-base'>
-                    <a href='#About' className='mr-5 hover:text-blue-500 duration-300'>About</a>
+                    <a href='#home' className='mr-5 hover:text-blue-500 duration-300'>About</a>
                     <a href='#GitHub' className='hover:text-blue-500 duration-300'>GitHub</a>
                 </nav>
             </div>
