f247e3bf5773c059086798e3fa4a30a64e396738
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-09-17 11:37:02 +0000

    installed prettier

--- portfolio-page/app/components/header.tsx
+++ portfolio-page/app/components/header.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
 
-export default function Header(){
-    return(
-        <header className='text-gray-700 border-b border-gray-200'>
-            <div className='container flex flex-col mx-auto p-5 md:flex-row items-center'>
-                <a href='#' className='font-medium text-gray-900 mb-4 md:mb-0'>
-                    <span className='text-xl ml-0 md:ml-3'>codsalmoon</span>
-                </a>
-                <nav className='md:ml-auto text-base'>
-                    <a href='#home' className='mr-5 hover:text-blue-500 duration-300'>About</a>
-                    <a href='#GitHub' className='hover:text-blue-500 duration-300'>GitHub</a>
-                </nav>
-            </div>
-        </header>
-    );
+export default function Header() {
+  return (
+    <header className="text-gray-700 border-b border-gray-200">
+      <div className="container flex flex-col mx-auto p-5 md:flex-row items-center">
+        <a href="#" className="font-medium text-gray-900 mb-4 md:mb-0">
+          <span className="text-xl ml-0 md:ml-3">codsalmoon</span>
+        </a>
+        <nav className="md:ml-auto text-base">
+          <a href="#home" className="mr-5 hover:text-blue-500 duration-300">
+            About
+          </a>
+          <a href="#GitHub" className="hover:text-blue-500 duration-300">
+            GitHub
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
--- portfolio-page/app/page.tsx
+++ portfolio-page/app/page.tsx
@@ -1,29 +1,32 @@
 import Header from "@/app/components/header";
 import Image from "next/image";
-import profilePic from "@/app/public/img/230912_4.jpg"
+import profilePic from "@/app/public/img/230912_4.jpg";
 
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
       <div className="flex container mx-auto py-20 px-10 flex-col md:flex-row items-center">
-        <div className="text-gray-700 md:w-1/2 mb-16 lg:pr-24 md:pr-16 text-center md:text-left flex-grow" id="home">
+        <div
+          className="text-gray-700 md:w-1/2 mb-16 lg:pr-24 md:pr-16 text-center md:text-left flex-grow"
+          id="home"
+        >
           <h1 className="text-3xl font-bold sm:text-6xl">
-          Every people
-          <br/>
-          can be creative.
+            Every people
+            <br />
+            can be creative.
           </h1>
           <p className="text-gray-500 mb-8 leading-relaxed">
-            <br/>
+            <br />
             2021-2025
-            <br/>
+            <br />
             Faculty of Engineering, Iwate University
-            <br/>
+            <br />
             Systems Engineering Lab.
-            <br/>
-            <br/>
+            <br />
+            <br />
             2025-2027
-            <br/>
+            <br />
             Japan Advanced Institute of Science and Technology
           </p>
           <button className="text-white bg-indigo-600 py-2 px-6 border-0 rounded-full">
@@ -31,10 +34,7 @@ export default function Home() {
           </button>
         </div>
         <div className="md:w-1/2 lg:max-w-lg w-5/6">
-          <Image
-            src={profilePic}
-            alt="Image"
-          />
+          <Image src={profilePic} alt="Image" />
         </div>
       </div>
     </>
