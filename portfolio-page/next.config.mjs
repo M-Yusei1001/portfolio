571aab128e01a8b1ee64d10a0b81bffdcf04a74b
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-09-17 07:55:42 +0000

    second commit

---
+++ portfolio-page/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode:true,
+    webpack:(config,{dev})=>{
+        if(dev){
+            config.watchOptions = {
+                poll:1000,
+                aggregateTimeout:200,
+            };
+        }
+        return config;
+    }
+};
+
+export default nextConfig;
